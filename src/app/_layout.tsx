83535d46e8a586725048774b4cbfc1699ce21e3c
Vitinn089 <vitorlopes089@gmail.com>
2024-12-11 11:49:26 -0300

    🚀 NLW Pocket: Aula 2

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import  { Stack } from "expo-router"
 import { colors } from "@/styles/theme"
 
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
 import { 
     useFonts,
     Rubik_600SemiBold,
@@ -22,10 +24,14 @@ export default function Layout() {
     if(!fontsLoaded)
         return <Loading />
 
-    return <Stack 
-        screenOptions={{
-            headerShown: false,
-            contentStyle: {backgroundColor: colors.gray[100]}
-        }} 
-    />
+    return(
+    <GestureHandlerRootView style={{flex: 1}}>
+         <Stack
+            screenOptions={{
+                headerShown: false,
+                contentStyle: {backgroundColor: colors.gray[100]}
+            }}
+        />
+    </GestureHandlerRootView>
+    )
 }
